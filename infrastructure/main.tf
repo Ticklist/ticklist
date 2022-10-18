6def648f78ece666a13128238b8851ff690cac04
Matthaeus Heer <matthaeusheer@gmail.com>
2022-10-18 18:57:08 +0200

    added tf backend config and tests tf against Azure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,17 +5,32 @@ terraform {
       version = "~>2.31.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-ticklist-backend"
+    storage_account_name = "storageticklistbackend"
+    container_name       = "terraform-state"
+    key                  = "ticklist.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 }
 
+locals {
+  tags = {
+    "expireOn" = "2022-03-25"
+    "owner"    = "matthaeus.heer@ipt.ch, matthias.staehli@ipt.ch"
+  }
+}
+
 resource "azurerm_resource_group" "rg-ticklist" {
   name     = "rg-ticklist"
-  location = "swz-north"
-  tags = {
-    owner        = "matthaeus.heer@ipt.ch, matthias.staehli@ipt.ch"
-    source      = "Terraform"
+  location = "switzerlandnorth"
+  tags     = local.tags
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
   }
 }
